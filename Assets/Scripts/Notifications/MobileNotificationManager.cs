f7686b42eddedd378c16a8ddbc47b5804eed56b4
StonedBoi <75484259+StonedBoi@users.noreply.github.com>
2022-05-31 16:13:44 +0200

    Start on notifications

---
+++ Assets/Scripts/Notifications/MobileNotificationManager.cs
@@ -0,0 +1,55 @@
+    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_ANDROID
+using Unity.Notifications.Android;
+#endif
+
+public class MobileNotificationManager : MonoBehaviour
+{
+    #if UNITY_ANDROID
+    public AndroidNotificationChannel defaultNotificationChannel;
+
+    private int identifier;
+
+    private void Start()
+    {
+        defaultNotificationChannel = new AndroidNotificationChannel()
+        {
+            Id = "default_channel",
+            Name = "Default Channel",
+            Description = "For Generic notifications",
+            Importance = Importance.Default,
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(defaultNotificationChannel);
+
+        AndroidNotification notification = new AndroidNotification()
+        {
+            Title = "Test Notification!",
+            Text = "This is a test notification!",
+            SmallIcon = "default",
+            LargeIcon = "default",
+            FireTime = System.DateTime.Now.AddSeconds(10),
+        };
+
+        identifier = AndroidNotificationCenter.SendNotification(notification, "default_channel");
+
+        AndroidNotificationCenter.NotificationReceivedCallback receivedNotificationHandler = delegate (AndroidNotificationIntentData data)
+        {
+            var msg = "Notification received : " + data.Id + "\n";
+            msg += "\n Notification received: ";
+            msg += "\n .Title: " + data.Notification.Title;
+            msg += "\n .Body: " + data.Notification.Text;
+            msg += "\n .Channel: " + data.Channel;
+            Debug.Log(msg);
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
